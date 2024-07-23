17c656a973c6127f85825f5b365e449c2a9fa389
Saberslay <dansteamshell@gmail.com>
2024-07-23 06:37:10 +0100

    remover a commented out line

--- src/main/java/me/saberslay/owOChatEnhancer/OwOChatEnhancer.java
+++ src/main/java/me/saberslay/owOChatEnhancer/OwOChatEnhancer.java
@@ -7,7 +7,6 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class OwOChatEnhancer extends JavaPlugin implements Listener  {
-    private boolean msgOpUpdate = false;
     @Override
     public void onEnable() {
         // Register the event listener
